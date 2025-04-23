20fbd8f288e89858aa2268770314ced2d6d2bc66
Ahmadd01211 <ahmad12399678@gmail.com>
2025-04-24 03:43:24 +0500

    frontend start

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
+// import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
 
@@ -12,9 +12,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
